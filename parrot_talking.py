9957003acaa5b2802d4c2fe0d9fd1ed335c3c150
rgunduz01 <81578189+rgunduz01@users.noreply.github.com>
2021-05-07 20:36:25 +0300

    Update parrot_talking.py

--- parrot_talking.py
+++ parrot_talking.py
@@ -1,3 +1,14 @@
+#We have a loud talking parrot. We are in trouble if the parrot is talking and the hour is before 6 or after 21.
+#Define a function taking two parameters (talking and hour) to return True if we are in trouble. The argument to  talking parameter can only be True or False whether it is talking or not. The argument to hour parameter should be the current hour time in the range of 0 to 23.
+#For example:
+
+#Test	Result
+#print(parrot_trouble(True, 5))
+#True
+#print(parrot_trouble(True, 8))
+#False
+#print(parrot_trouble(False, 22))
+#False
 def parrot_trouble(talking, hour):
   if hour in range(1,24):
    return talking==True and (hour<7 or hour>20)
